b2a67f996876719749bd18aefc92e982fc751503
tamizhmani2000 <tamizhmani2000@yahoo.com>
2017-02-22 16:55:47 -0600

    Initial Setup

---
+++ CatalogManagement/src/main/java/com/catalog/CatalogManagementApplication.java
@@ -0,0 +1,12 @@
+package com.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogManagementApplication.class, args);
+	}
+}
